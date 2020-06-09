f718289b285450898634f8e4daadfcb0725b17d4
rachel0915 <wegenerr22@greenhill.org>
2020-06-09 14:52:39 -0500

    Change size

--- src/com/company/Challenge2RachelV2.java
+++ src/com/company/Challenge2RachelV2.java
@@ -3,7 +3,7 @@ package com.company;
 Used with loops
  06/07/2020*/
 public class Challenge2RachelV2 {
-
+public static final int SIZE = 7;
     public static void main(String[] args) { //MAIN FUNCTION
     //Challenge 2 with loops
         Line();
@@ -17,18 +17,18 @@ public class Challenge2RachelV2 {
 
     //LOOPS START DOWN HERE
 
-    public static void Line(){
+    public static void Line(){ //This prints the line pattern
         System.out.print("+");
-        for(int dash = 1 ; dash <= 9 ; dash++){
+        for(int dash = 1 ; dash <= 2*SIZE+1 ; dash++){
             System.out.print("-");
         }
         System.out.println("+");
     }
 
-    public static void Top() {
-        for(int line = 1 ; line <= 4 ; line++){
+    public static void Top() { //This prints the top section of the pattern
+        for(int line = 1 ; line <= SIZE ; line++){
             System.out.print("|");
-            for(int space = 1 ; space <= -1 * line + 5 ; space++){
+            for(int space = 1 ; space <= -1 * line + SIZE +1 ; space++){
                 System.out.print(" ");
             }
             for(int fslash = 1 ; fslash <= 1 * line - 1 ; fslash++){
@@ -38,24 +38,24 @@ public class Challenge2RachelV2 {
             for(int bslash = 1 ; bslash <= 1 * line - 1 ; bslash++){
                 System.out.print("\\");
             }
-            for(int space = 1 ; space <= -1 * line + 5 ; space++){
+            for(int space = 1 ; space <= -1 * line + SIZE +1 ; space++){
                 System.out.print(" ");
             }
             System.out.println("|");
         }
     }
 
-    public static void Bottom() {
-        for(int line = 1 ; line <= 4 ; line++){
+    public static void Bottom() { //This prints the bottom section of the pattern
+        for(int line = 1 ; line <= SIZE ; line++){
             System.out.print("|");
             for(int space = 1 ; space <= 1 * line ; space++){
                 System.out.print(" ");
             }
-            for(int bslash = 1 ; bslash <= -1 * line + 4 ; bslash++){
+            for(int bslash = 1 ; bslash <= -1 * line + SIZE ; bslash++){
                 System.out.print("\\");
             }
             System.out.print("*");
-            for(int fslash = 1 ; fslash <= -1 * line + 4 ; fslash++){
+            for(int fslash = 1 ; fslash <= -1 * line + SIZE ; fslash++){
                 System.out.print("/");
             }
             for(int space = 1 ; space <= 1 * line ; space++){
