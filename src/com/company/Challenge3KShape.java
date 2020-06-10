3fb525641d24dd8f243fe5ba9421a7b1462c552a
rachel0915 <wegenerr22@greenhill.org>
2020-06-09 21:43:44 -0500

    due 6/9 with sizing

--- src/com/company/Challenge3KShape.java
+++ src/com/company/Challenge3KShape.java
@@ -4,16 +4,16 @@ public class Challenge3KShape {
     //PSEUDO-CODE
     //top:create for loop letter=65 letter<=70 letter++
     //bottom: reverse for loop
-
+    public static final int SIZE = 6;
     public static void main(String[] args){
         char beginning=65;
-        for(int line=6 ; line > 0 ; line--){
+        for(int line=SIZE ; line > 0 ; line--){
             for(int let = 0; let < line ; let++){
                 System.out.print((char)(beginning + let) + " ");
             }
             System.out.println();
         }
-        for(int line=1 ; line <=6 ; line++){
+        for(int line=1 ; line <= SIZE ; line++){
             for(int let = 0 ; let < line ; let++){
                 System.out.print((char)(beginning + let) + " ");
             }
