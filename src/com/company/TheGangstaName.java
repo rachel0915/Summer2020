96bca9efd956948d1570ae5cd303a3b96401391e
rachel0915 <wegenerr22@greenhill.org>
2020-06-10 22:27:58 -0500

    gangsta name

--- src/com/company/TheGangstaName.java
+++ src/com/company/TheGangstaName.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.*;
 /*Write a program that outputs a person's "gangsta name."
 –first initial
 –Diddy
@@ -16,12 +17,36 @@ Print variable + ". Diddy" + last name in uppercase + first name + -izzle
  */
 public class TheGangstaName {
 
-    public static void main(String[] args){
-        String firstName = "Rachel";
-        String lastName = "Wegener";
-        char letter = firstName.charAt(0);
-        System.out.print(letter + ". Diddy " + lastName.toUpperCase() + " " + firstName + "-izzle");
+    public static void main(String[] args){ //MAIN FUNCTION
+        String fullName = "Rachel Wegener";
+        System.out.print("Your gangsta name is ");
+        initial(fullName);
+        System.out.print(". Diddy");
+        lastName(fullName);
+        firstName(fullName);
+        System.out.print("-izzle");
+    }
 
+
+    public static void initial(String fullName ){ //SUB FUNCTION
+        char letter = fullName.charAt(0);
+        System.out.print(letter);
+    }
+
+    public static void firstName(String fullName){
+        int indexOfSpace = fullName.indexOf(" ");
+        String firstName = fullName.substring(0, indexOfSpace);
+        System.out.print(" " + firstName);
+
+
+    }
+    public static void lastName(String fullName){
+        int indexOfSpace = fullName.indexOf(" ");
+        if (indexOfSpace > 0){
+            String lastName = fullName.substring(indexOfSpace);
+            lastName = lastName.toUpperCase();
+            System.out.print(lastName);
+        }
     }
 
 }
