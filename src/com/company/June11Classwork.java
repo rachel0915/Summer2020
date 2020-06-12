69344130b90a7c4d0ee9c605b714323fd64645b6
rachel0915 <wegenerr22@greenhill.org>
2020-06-12 16:03:26 -0500

    finish rest of message

---
+++ src/com/company/June11Classwork.java
@@ -0,0 +1,51 @@
+package com.company;
+import java.util.*;
+
+public class June11Classwork {
+    public static Scanner userPhrase = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Type your message: ");
+        String message = userPhrase.nextLine();
+        firstWord(message);
+
+    }
+
+    public static void firstWord(String message) {
+        int indexOf = message.indexOf(" ");
+        String firstWord = message.substring(0, indexOf + 1);
+        // message = message - firstWord
+        char letter;
+        int count = 0;
+        for (int index = 0; index < firstWord.length(); index++) {
+            letter = firstWord.charAt(index);
+            if (letter != ' ') {
+                count++;
+                String temp = firstWord.substring(index + 1);
+                indexOf = temp.indexOf(letter);
+                while (indexOf >= 0) {
+                    count++;
+                    //       System.out.println(temp + " " + indexOf);
+                    temp = temp.substring(indexOf + 1);
+                    indexOf = temp.indexOf(letter);
+                }
+                // find rest of the letters in message
+                count = restProg(letter, message, count);
+                firstWord = firstWord.replace(letter, ' ');
+                System.out.println(letter + " " + count);
+                count = 0;
+            }
+        }
+
+    }
+    public static int restProg(char letter, String message, int count){
+        //solve here
+        return count;
+    }
+
+}
+
+
+
+
+
