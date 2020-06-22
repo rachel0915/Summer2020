36959f534ab20dc85767b6412425715dac926e06
rachel0915 <wegenerr22@greenhill.org>
2020-06-22 12:34:58 -0500

    6/21 assignment

--- src/com/company/StringsChallenge.java
+++ src/com/company/StringsChallenge.java
@@ -19,14 +19,16 @@ public class StringsChallenge {
         do {
             System.out.println("How many Strings would you like?");
             numOfStrings = userInput.nextInt();  // integer value user types in
+        }while(numOfStrings<=0 && numOfStrings>=11); //constraints for integer value
 
-            for (int loop = 0; loop<= numOfStrings; loop++) { //for loop controlled by int value
+
+        for (int loop = 0; loop< numOfStrings; loop++) { //for loop controlled by int value
                 System.out.println("Enter a String");
                 String stringInput = userInput.next(); //user inputs a word to be sent to the method
                 String receiveWord = output(stringInput); //returns from method
                 System.out.println(receiveWord);
             }
-        }while(numOfStrings>0 && numOfStrings<10); //constraints for integer value
+
 
 
     }
@@ -35,18 +37,17 @@ public class StringsChallenge {
     public static String output(String stringInput){
         String evenString = "";
         String oddString = "";
-        String finalOutput;
         int stringLength = stringInput.length(); //number of indexes in word
-        for(int evenChars = 0; evenChars <= stringLength; evenChars = evenChars + 2){
+        for(int evenChars = 0; evenChars < stringLength; evenChars += 2){
             char outputEven = stringInput.charAt(evenChars);
             evenString = evenString+outputEven;
         }
-        for(int oddChars = 1; oddChars <= stringLength; oddChars = oddChars+ 2){
+        for(int oddChars = 1; oddChars < stringLength; oddChars += 2){
             char outputOdd = stringInput.charAt(oddChars);
             oddString = oddString+outputOdd;
         }
-        finalOutput = evenString+" "+oddString;
-        return finalOutput;
+        return (evenString+" "+oddString);
+
     }
 
 
